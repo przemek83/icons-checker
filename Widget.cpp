3f4dea9031203fdea2d92b309f13195e82fc4c63
Przemek <4788832+przemek83@users.noreply.github.com>
2025-01-30 20:53:13 +0100

    Initial commit
    Import of existing project.

---
+++ Widget.cpp
@@ -0,0 +1,29 @@
+#include <QGridLayout>
+#include <QToolButton>
+
+#include "Widget.h"
+#include "ui_Widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    QGridLayout* layout = new QGridLayout(this);
+
+    setLayout(layout);
+
+    for(int i = 0; i < 70; ++i )
+    {
+        QToolButton* button = new QToolButton(this);
+        button->setIcon(QApplication::style()->standardIcon((QStyle::StandardPixmap)i));
+        button->setToolTip(QString::number(i));
+        layout->addWidget(button, i/6, i%6);
+    }
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
