99dcac66fa7da3eb6272e1ee9cc0ef14bd1a6b44
przemek83 <4788832+przemek83@users.noreply.github.com>
2025-02-01 08:32:54 +0100

    Some fixes in Widget header.

--- Widget.h
+++ Widget.h
@@ -3,20 +3,21 @@
 
 #include <QWidget>
 
-namespace Ui {
+namespace Ui
+{
 class Widget;
-}
+}  // namespace Ui
 
 class Widget : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit Widget(QWidget *parent = 0);
-    ~Widget();
+    explicit Widget(QWidget* parent = 0);
+    ~Widget() override;
 
 private:
-    Ui::Widget *ui;
+    Ui::Widget* ui;
 };
 
-#endif // WIDGET_H
+#endif  // WIDGET_H
